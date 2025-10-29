d926913477da3d65bd3b3ee1815ba7cb7a03d851
Ayyappa Bochula <ayyappabochula43@gmail.com>
2025-10-29 13:26:24 +0530

    Added Render domain to ALLOWED_HOSTS

--- demo1/settings.py
+++ demo1/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)n9dmsk0j0ju=jc1+^w^clh*3$dwjp@!kyp&0il)8js)e*olfw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
